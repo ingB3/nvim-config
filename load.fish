4206aef30a1eb25bd9c7b35d0bfe4accccb4b566
ingBE-macbook <chlgyrb111@uos.ac.kr>
2024-09-23 16:21:34 +0900

    Update load.fish

---
+++ load.fish
@@ -0,0 +1,17 @@
+# fish load.fish
+
+echo mkdir ~/.backup
+mkdir ~/.backup
+
+echo cp vimrc ~/.vimrc
+cp ~/.vimrc ~/.backup/
+cp ./vimrc ~/.vimrc
+
+echo cp -r ./local/share/nvim/site/autoload/airline/themes ~/.local/share/nvim/site/autoload/airline/
+cp -r ~/.local/share/nvim/site/autoload/airline ~/.backup/local-share-nvim-site-autoload-airline
+cp -r ./local/share/nvim/site/autoload/airline/themes ~/.local/share/nvim/site/autoload/airline/
+
+echo cp ./config/nvim/* ~/.config/nvim/
+cp -r ~/.config/nvim ~/.backup
+cp ./config/nvim/* ~/.config/nvim/
+

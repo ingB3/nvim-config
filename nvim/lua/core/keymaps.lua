0dd0154f90323fa71e03c37e6d5ef3e1b5cb4032
ingBE-macbook <chlgyrb111@uos.ac.kr>
2025-08-11 15:20:57 +0200

    Update keymaps of arrow to move buffer and tab

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -12,3 +12,13 @@ map('n', '<leader>n', functions.toggleRelativeNumber)
 -- Indent
 map('v', '<', '<gv')
 map('v', '>', '>gv')
+
+-- Buffer navigation
+map('n', '<Left>', ':bprevious<CR>')
+map('n', '<Right>', ':bnext<CR>')
+
+-- Tab navigation
+map('n', '<Up>', ':tabprevious<CR>')
+map('n', '<Down>', ':tabnext<CR>')
+-- map('n', 'gn', ':tabnew<CR>')
+-- map('n', 'gn', ':tabclose<CR>')
